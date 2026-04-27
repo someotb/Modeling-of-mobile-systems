a3a6526a0929a81390efd07f85c27cf0c9d7c203
someotb <llyryk934@gmail.com>
2026-04-27 22:52:09 +0700

    feat(gui): add imgui and implot

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include "fft.hpp"
 #include "sharedData.hpp"
 
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "implot.h"
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
 #include <thread>
 
 void run_backend(sharedData &sh_data)
@@ -118,7 +124,7 @@ void run_backend(sharedData &sh_data)
     for (size_t i = 0; i < tx.size(); ++i)
         std::cout << tx[i] << " ";
 
-        
+
     /*
     Transmission medium
     */
@@ -148,13 +154,73 @@ void run_backend(sharedData &sh_data)
     std::cout << "After Hamming decode: " << hamming_decoded << "\n";
 }
 
+void run_gui(sharedData &sh_data)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow(
+        "MMS",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // Твоё окно
+        ImGui::Begin("MMS Panel");
+        ImGui::Text("Hello!");
+        // сюда будешь добавлять графики, кнопки и тд
+        ImGui::End();
+
+        ImGui::Render();
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main()
 {
     sharedData sh_data;
     std::thread backend_t(run_backend, std::ref(sh_data));
+    std::thread gui_t(run_gui, std::ref(sh_data));
 
     if (backend_t.joinable())
         backend_t.join();
+    if (gui_t.joinable())
+        gui_t.join();
 
     return 0;
 }
